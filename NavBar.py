0e03af30a91e454b6f1b90cc456234a3359e7481
PercyODI <pearse.2008@gmail.com>
2019-08-04 17:39:54 -0500

    NavBar shoing next/previous buttons

--- NavBar.py
+++ NavBar.py
@@ -1,13 +1,38 @@
-from tkinter import Frame
+from tkinter import Frame, Button, E, W, BOTH
 from Util import PhSize
+from math import floor
 
 class NavBar:
     def __init__(self, masterFrame: Frame):
         self.masterFrame = masterFrame
         self.masterFrame["bg"] = "black"
+        self.masterFrame["highlightbackground"] = "blue"
+        self.masterFrame["highlightcolor"] = "blue"
+        self.masterFrame["highlightthickness"] = 1
+
+        self.nextButtonFrame = Frame(self.masterFrame)
+        self.nextButtonFrame.pack_propagate(0)
+        self.nextButtonFrame.grid(row=0, column=1, sticky=E, padx=0, pady=0)
+
+        self.nextButton = Button(self.nextButtonFrame, text="Next")
+        self.nextButton.pack(fill=BOTH, expand=1)
+        # self.nextButton.grid(row=0, column=1, sticky=E)
+
+        self.previousButtonFrame = Frame(self.masterFrame)
+        self.previousButtonFrame.pack_propagate(0)
+        self.previousButtonFrame.grid(row=0, column=0, sticky=W, padx=0, pady=0)
+
+        self.previousButton = Button(self.previousButtonFrame, text="Previous")
+        self.previousButton.pack(fill=BOTH, expand=1)
+        # self.previousButton.grid(row=0, column=0, sticky=W)
 
     def resize(self, newSize: PhSize):
         self.masterFrame["width"] = newSize.width
         self.masterFrame["height"] = newSize.height
-            
+
+        self.nextButtonFrame["width"] = floor(newSize.width / 2)
+        self.previousButtonFrame["width"] = floor(newSize.width / 2)
+        
+        self.nextButtonFrame["height"] = newSize.height
+        self.previousButtonFrame["height"] = newSize.height
         print("NavBar", "Width:", newSize.width, "Height:", newSize.height)
--- PdfManager.py
+++ PdfManager.py
@@ -16,23 +16,28 @@ class PdfManager:
         self.leftPdfFrame = PdfFrame(self.leftPane, PageSide.LEFT)
         self.leftPdfFrame.DrawPage(self.doc[self.currentLeftPageNum])
 
-        self.rightPane = Frame(master)
-        self.rightPane.grid(row=0, column=1)
-        self.rightPane["bg"] = "red"
-        self.rightPdfFrame = PdfFrame(self.rightPane, PageSide.RIGHT)
-        self.rightPdfFrame.DrawPage(self.doc[self.currentRightPageNum])
+        if(self.doc.pageCount > 1):
+            self.rightPane = Frame(master)
+            self.rightPane.grid(row=0, column=1)
+            self.rightPane["bg"] = "red"
+            self.rightPdfFrame = PdfFrame(self.rightPane, PageSide.RIGHT)
+            self.rightPdfFrame.DrawPage(self.doc[self.currentRightPageNum])
+        else:
+            self.rightPdfFrame = None
 
 
     def NextPage(self):
-        pass
+        if(self.currentLeftPageNum + 1 < self.doc.pageCount):
+            self.currentLeftPageNum += 1
+            self.leftPdfFrame.DrawPage(self.doc[self.currentLeftPageNum])
+        
+        if(self.currentRightPageNum + 1 < self.doc.pageCount):
+            self.currentRightPageNum += 1
+            self.rightPdfFrame.DrawPage(self.doc[self.currentRightPageNum])
 
     def resize(self, newSize: PhSize):
         self.leftPdfFrame.resize(PhSize(newSize.width / 2, newSize.height))
-        # self.leftPane["width"] = newSize.width / 2
-        # self.leftPane["height"] = newSize.height
-
-        # self.rightPane["width"] = newSize.width / 2
-        # self.rightPane["height"] = newSize.height
-        self.rightPdfFrame.resize(PhSize(newSize.width / 2, newSize.height))
+        if(self.rightPdfFrame is not None):
+            self.rightPdfFrame.resize(PhSize(newSize.width / 2, newSize.height))
 
         print("PdfManager", "Width:", newSize.width, "Height:", newSize.height)

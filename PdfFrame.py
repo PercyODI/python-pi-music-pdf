310d62d39000c5badde4b2bf90d72ef99e23fe4b
PercyODI <pearse.2008@gmail.com>
2019-08-04 16:50:21 -0500

    Pdfs being rendered in window

--- PdfFrame.py
+++ PdfFrame.py
@@ -1,6 +1,42 @@
-class PdfFrame:
-    def __init__(self, frame):
-        self.frame = frame
+from tkinter import *
+from PIL import Image, ImageTk
+import fitz
+from Util import PhSize
+from enum import Enum, auto
 
-    def DrawPdf(self):
-        pass
+class PageSide(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+
+class PdfFrame:
+    def __init__(self, masterFrame: Frame, pageSide: PageSide):
+        self.masterFrame = masterFrame
+        self.pdfCanvas = Canvas(self.masterFrame, width=1, height=1)
+        self.pdfCanvas.grid(column=0, row=0)
+        self.pdfPage = None
+        self.canvasPdfId = None
+        self.pageSide = pageSide
+
+    def DrawPage(self, pdfPage: fitz.Page):
+        self.pdfPage = pdfPage
+        if(self.canvasPdfId is not None):
+            self.pdfCanvas.delete(self.canvasPdfId)
+        shrinkFactor = min(
+            int(self.pdfCanvas["height"]) / pdfPage.rect.height,
+            int(self.pdfCanvas["width"]) / pdfPage.rect.width)
+        mat = fitz.Matrix(shrinkFactor, shrinkFactor)
+        pix = pdfPage.getPixmap(matrix=mat)
+
+        mode = "RGBA" if pix.alpha else "RGB"
+        self.pdfCanvas.rawImage = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
+        self.pdfCanvas.image = ImageTk.PhotoImage(self.pdfCanvas.rawImage)
+        if self.pageSide is PageSide.RIGHT:
+            self.canvasPdfId = self.pdfCanvas.create_image(0, 0, anchor=NW, image=self.pdfCanvas.image)
+        else:
+            self.canvasPdfId = self.pdfCanvas.create_image(self.pdfCanvas["width"], 0, anchor=NE, image=self.pdfCanvas.image)
+    
+    def resize(self, newSize: PhSize):
+        self.pdfCanvas["width"] = newSize.width
+        self.pdfCanvas["height"] = newSize.height
+        if(self.pdfPage is not None):
+            self.DrawPage(self.pdfPage)
--- PdfManager.py
+++ PdfManager.py
@@ -1,10 +1,11 @@
 import fitz
 from tkinter import *
-from PdfFrame import PdfFrame
+from PdfFrame import PdfFrame, PageSide
 from Util import PhSize
 
 class PdfManager:
     def __init__(self, master: Frame, pdfPath: str):
+        self.masterFrame = master
         self.doc = fitz.Document(pdfPath)
         self.currentLeftPageNum = 0
         self.currentRightPageNum = 1
@@ -12,23 +13,26 @@ class PdfManager:
         self.leftPane = Frame(master)
         self.leftPane.grid(row=0, column=0)
         self.leftPane["bg"] = "light blue"
-        self.leftPdfFrame = PdfFrame(self.leftPane)
+        self.leftPdfFrame = PdfFrame(self.leftPane, PageSide.LEFT)
+        self.leftPdfFrame.DrawPage(self.doc[self.currentLeftPageNum])
 
         self.rightPane = Frame(master)
         self.rightPane.grid(row=0, column=1)
         self.rightPane["bg"] = "red"
-        self.rightPdfFrame = PdfFrame(self.rightPane)
+        self.rightPdfFrame = PdfFrame(self.rightPane, PageSide.RIGHT)
+        self.rightPdfFrame.DrawPage(self.doc[self.currentRightPageNum])
 
-        self.masterFrame = master
 
     def NextPage(self):
         pass
 
     def resize(self, newSize: PhSize):
-        self.leftPane["width"] = newSize.width / 2
-        self.leftPane["height"] = newSize.height
+        self.leftPdfFrame.resize(PhSize(newSize.width / 2, newSize.height))
+        # self.leftPane["width"] = newSize.width / 2
+        # self.leftPane["height"] = newSize.height
 
-        self.rightPane["width"] = newSize.width / 2
-        self.rightPane["height"] = newSize.height
+        # self.rightPane["width"] = newSize.width / 2
+        # self.rightPane["height"] = newSize.height
+        self.rightPdfFrame.resize(PhSize(newSize.width / 2, newSize.height))
 
         print("PdfManager", "Width:", newSize.width, "Height:", newSize.height)
